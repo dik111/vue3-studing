2470167493f20558f011ef51431e58d6c5727f2e
yuweiZhan <195812208@qq.com>
2020-10-29 21:47:55 +0800

    :sparkles: setup以及ref函数

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <h2>欢迎光临</h2>
+  <div>请选择一位美女为你服务</div>
+  <div>
+    <button v-for="(item,index) in girls" :key="index" @click="selectGirlFun(index)">
+      {{ index }}:{{ item }}
+    </button>
+    <div>你选择了哪个美女【{{ selectGirl }}】为你服务</div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const girls = ref(["大脚", "刘英", "晓红"])
+    const selectGirl = ref("")
+    const selectGirlFun = (index: number) => {
+        selectGirl.value = girls.value[index]
+    }
+    return {
+      girls, selectGirl,selectGirlFun
+    }
+  }
 });
 </script>
 
